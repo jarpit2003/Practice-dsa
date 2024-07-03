cadb0eb47a57ff669ce336f5f7707a0578810e2d
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-07-03 23:39:21 +0530

    Time: 56 ms (93.79%), Space: 37.7 MB (95.34%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,23 +1,23 @@
-class Solution {
-    private:
 
+class Solution {
 public:
     int minDifference(vector<int>& nums) {
         int n = nums.size();
-    sort(begin(nums),end(nums));
-        if(n<=3) 
-        {
+        if (n <= 4) {
             return 0;
         }
-        else
-        {
+        
+        // Sort the first 4 and the last 4 elements
+        partial_sort(begin(nums), begin(nums) + 4, end(nums));
+        nth_element(begin(nums) + 4, begin(nums) + (n - 4), end(nums));
+        sort(begin(nums) + (n - 4), end(nums));
+        
         int result = INT_MAX;
-        result = min(result,nums[n-4]-nums[0]);
-        result = min(result,nums[n-1]-nums[3]);
-        result = min(result,nums[n-2]-nums[2]);
-        result = min(result,nums[n-3]-nums[1]);
+        result = min(result, nums[n - 4] - nums[0]); // Change the three smallest elements
+        result = min(result, nums[n - 1] - nums[3]); // Change the three largest elements
+        result = min(result, nums[n - 2] - nums[2]); // Change the largest and two smallest elements
+        result = min(result, nums[n - 3] - nums[1]); // Change the smallest and two largest elements
+        
         return result;
-        }
     }
 };
-/*O(nlogn);O(1)*/
