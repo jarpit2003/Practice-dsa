5d187c9db4b402932d914cb454453a299d48af35
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-08-15 23:30:49 +0530

    Time: 56 ms (96.67%), Space: 86 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fived = 0;
-        int tend = 0;
-        int twentyd = 0;
-        bool flag = true;
-        for(int i = 0;i<bills.size();++i)
-        {
-            if(bills[i]==5)
-            {
+        int fived = 0, tend = 0;
+        
+        for (int bill : bills) {
+            if (bill == 5) {
                 fived++;
-            }
-            else if(bills[i]==10)
-            {
-                tend++;
-                if(fived>0)
-                {
+            } 
+            else if (bill == 10) {
+                if (fived > 0) {
                     fived--;
+                    tend++;
+                } else {
+                    return false;  // Can't provide change
                 }
-                else
-                {
-                    flag = false;
-                   break; 
-                }
-            }
-            else if(bills[i]==20)
-            {
-                twentyd++;
-                if(tend>0&&fived>0)
-                {
+            } 
+            else if (bill == 20) {
+                // Prefer giving one $10 and one $5 as change
+                if (tend > 0 && fived > 0) {
                     tend--;
                     fived--;
-                }
-                else if(tend==0&&fived>=3)
-                {
-                    fived-=3;
-                }
-                else
-                {
-                    flag = false;
-                    break;
+                } 
+                // If no $10, give three $5 bills
+                else if (fived >= 3) {
+                    fived -= 3;
+                } 
+                else {
+                    return false;  // Can't provide change
                 }
             }
         }
-        return flag;
+        
+        return true;
     }
 };
