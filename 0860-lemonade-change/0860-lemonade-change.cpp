2f84f71828d4ac9c733bb66ac75a32395a23b7fe
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-08-15 23:34:06 +0530

    Time: 90 ms (5.56%), Space: 86 MB (50.86%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -4,30 +4,34 @@ public:
         int fived = 0, tend = 0;
         
         for (int bill : bills) {
-            if (bill == 5) {
-                fived++;
-            } 
-            else if (bill == 10) {
-                if (fived > 0) {
-                    fived--;
-                    tend++;
-                } else {
-                    return false;  // Can't provide change
-                }
-            } 
-            else if (bill == 20) {
-                // Prefer giving one $10 and one $5 as change
-                if (tend > 0 && fived > 0) {
-                    tend--;
-                    fived--;
-                } 
-                // If no $10, give three $5 bills
-                else if (fived >= 3) {
-                    fived -= 3;
-                } 
-                else {
-                    return false;  // Can't provide change
-                }
+            switch(bill) {
+                case 5:
+                    fived++;
+                    break;
+                    
+                case 10:
+                    if (fived > 0) {
+                        fived--;
+                        tend++;
+                    } else {
+                        return false;  // Can't provide change
+                    }
+                    break;
+                    
+                case 20:
+                    if (tend > 0 && fived > 0) {
+                        tend--;
+                        fived--;
+                    } else if (fived >= 3) {
+                        fived -= 3;
+                    } else {
+                        return false;  // Can't provide change
+                    }
+                    break;
+                    
+                default:
+                    // Handle unexpected values if needed
+                    return false;
             }
         }
         
