28607694e57fdc5878eef232c8a13d91c60d0ee6
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 19:37:10 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (41.04%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -3,11 +3,6 @@ public:
     int minBitFlips(int start, int goal) {
         /*traverse where there is achange there make a chnge increase the count*/
        int xorResult = start ^goal;
-        int count = 0;
-        while(xorResult){
-            xorResult &= (xorResult - 1);
-            count++;
-        }
-     return count;
+        return __builtin_popcount(xorResult);
     }
 };
