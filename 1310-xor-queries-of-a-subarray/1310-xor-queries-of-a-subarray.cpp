86a43d930a5724c8333b8db707d49dc60e1e5be0
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 12:40:12 +0530

    Time: 60 ms (61.22%), Space: 41.8 MB (72.15%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        int n = arr.size();
-        vector<int> cumXor(n,0);
-        
-        // Build the prefix XOR array in one pass
+       int n = arr.size();
+        vector<int>cumXor(n,0);
         cumXor[0] = arr[0];
-        for (int i = 1; i < n; ++i) {
-            cumXor[i] = cumXor[i - 1] ^ arr[i];
+        for(int i =1;i<n;i++){
+            cumXor[i] = cumXor[i-1]^arr[i];
         }
-
-        // Reserve space to avoid multiple reallocations in the result vector
         vector<int> result;
         result.reserve(queries.size());
-
-        // Process queries
         for (const auto& query : queries) {
-            int left = query[0];
-            int right = query[1];
+            int temp1 = query[0];
+            int temp2 = query[1];
             
-            // Compute XOR for the range [left, right]
-            int xor_val = cumXor[right];
-            if (left > 0) {
-                xor_val ^= cumXor[left - 1];
-            }
+          int xor_val = cumXor[temp2]^(temp1 == 0 ? 0: cumXor[temp1-1]);
             
-            // Store the result
             result.push_back(xor_val);
         }
-
+        
         return result;
     }
 };
