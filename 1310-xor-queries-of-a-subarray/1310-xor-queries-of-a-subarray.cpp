ada1c14deb2fecadc533eaeb9da5f71941a420c0
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 12:08:29 +0530

    Time: 1416 ms (12.57%), Space: 41.8 MB (60.93%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        /* Declare a vector named result
-        Design A SYSTEM such that querirs[i][.first,.second]
-        calcute Xor and push in the array */
-         vector<int>result;
-        int temp1;
-        int temp2;
-        int Xored=arr[temp1];
-        for(int i = 0;i<queries.size();i++)
-        {
-         temp1 = queries[i][0];
-         temp2 = queries[i][1];
-         Xored =  arr[temp1];
-            for(int j = temp1+1;j<=temp2;j++)
-            {
-                Xored^=arr[j];
+        vector<int> result;
+        
+        for (const auto& query : queries) {
+            int temp1 = query[0];
+            int temp2 = query[1];
+            int Xored = arr[temp1];
+            
+            // XOR all elements between temp1 and temp2 (inclusive)
+            for (int j = temp1 + 1; j <= temp2; j++) {
+                Xored ^= arr[j];
             }
-         result.push_back(Xored);
+            
+            result.push_back(Xored);
         }
+        
         return result;
     }
 };
