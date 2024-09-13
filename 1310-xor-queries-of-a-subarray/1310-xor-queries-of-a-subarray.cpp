451ae411c4c1a79fb194e140b143050e30ec13a0
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 12:28:13 +0530

    Time: 56 ms (74.57%), Space: 42 MB (57.06%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+       int n = arr.size();
+        vector<int>cumXor(n,0);
+        cumXor[0] = arr[0];
+        for(int i =1;i<n;i++){
+            cumXor[i] = cumXor[i-1]^arr[i];
+        }
         vector<int> result;
         
         for (const auto& query : queries) {
             int temp1 = query[0];
             int temp2 = query[1];
-            int Xored = arr[temp1];
             
-            // XOR all elements between temp1 and temp2 (inclusive)
-            for (int j = temp1 + 1; j <= temp2; j++) {
-                Xored ^= arr[j];
-            }
+          int xor_val = cumXor[temp2]^(temp1 == 0 ? 0: cumXor[temp1-1]);
             
-            result.push_back(Xored);
+            result.push_back(xor_val);
         }
         
         return result;
