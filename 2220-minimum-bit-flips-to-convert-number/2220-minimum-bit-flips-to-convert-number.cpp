eff06ac55c83a18455cec7ba01a80a02c34f30b7
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 19:16:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.04%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start ^ goal);
+        /*traverse where there is achange there make a chnge increase the count*/
+        int count = 0;
+        bitset<32>bin1(start);
+        bitset<32>bin2(goal);
+        for(int i = 0;i<32;++i){
+            if(bin1[i]!=bin2[i]){
+                count++;
+            }
+        }
+     return count;
     }
 };
