ccc275c8d7a94b443d4cfc1ddfaf2de468546129
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 19:43:55 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (78.42%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int xorResult = start ^ goal;
-        int count = 0;
-
-        // Count the number of set bits (1s) manually
-        while (xorResult > 0) {
-            count += (xorResult & 1); // Increment count if the least significant bit is 1
-            xorResult >>= 1;          // Right shift to process the next bit
-        }
-
-        return count;
+        return __builtin_popcount(start ^ goal);
     }
 };
+
