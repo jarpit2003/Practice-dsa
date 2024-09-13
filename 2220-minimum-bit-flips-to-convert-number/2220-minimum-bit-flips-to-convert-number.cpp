c8066291aed13a1d5880ecf88ef51532f406046b
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-09-13 19:34:21 +0530

    Time: 3 ms (40.89%), Space: 7.6 MB (41.04%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -4,9 +4,9 @@ public:
         /*traverse where there is achange there make a chnge increase the count*/
        int xorResult = start ^goal;
         int count = 0;
-        while(xorResult>0){
-            count+= xorResult & 1;
-            xorResult >>=1;
+        while(xorResult){
+            xorResult &= (xorResult - 1);
+            count++;
         }
      return count;
     }
