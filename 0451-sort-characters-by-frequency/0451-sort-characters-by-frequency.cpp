d501d0a3fe46049bc8cb88e30ad3ffabd5c11217
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2024-11-02 18:07:15 +0530

    Time: 142 ms (5.13%), Space: 10.6 MB (89.12%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+      if(s.empty())
+      {
+          return "";
+      }
+      unordered_map<char,int>map;
+        for(int i =0;i<s.length();++i)
+        {
+            map[s[i]]++;
+        }
+        sort(s.begin(),s.end(),[&map](char a ,char b)
+             {
+                 if(map[a]==map[b])
+                 {
+                     return a>b;
+                 }
+                 else
+                 {
+                     return map[a]>map[b];
+                 }
+             });
+            return s;
+    }
+            
+};
