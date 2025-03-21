98b56672b8f5c13dca27bbc1b1eaff2738c1e7ba
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-03-21 16:48:38 +0530

    Time: 12 ms (32.6%), Space: 7.9 MB (35.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-      int sum = 0;
-       if(n>=2)
-       {
-        sum = sum+fib(n-1)+fib(n-2);
-       }
-       if(n==1)
-       {
-        sum = 1; 
-       }
-       if(n==0)
-       {
-        sum= 0;
-       }
-       return sum;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        else return fib(n-1)+fib(n-2);
     }
 };
