ad29be272ca0b1baf24a7582850ece8a8a060b3a
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-05-11 16:26:17 +0530

    Time: 19 ms (30.93%), Space: 13.5 MB (48.36%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,138 +1,77 @@
-/*class Solution {
+class Solution {
+    unordered_map<string, int> mpp; // Stores distance from beginWord
+    vector<vector<string>> ans;     // Stores all shortest sequences
+    string b;                       // Begin word
+    int sz;                         // Word length
+
+    void dfs(string word, vector<string>& seq) {
+        if (word == b) {
+            reverse(seq.begin(), seq.end());
+            ans.push_back(seq);
+            reverse(seq.begin(), seq.end());
+            return;
+        }
+        int steps = mpp[word];
+        for (int i = 0; i < sz; i++) {
+            char original = word[i];
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                word[i] = ch;
+                if (mpp.find(word) != mpp.end() && mpp[word] + 1 == steps) {
+                    seq.push_back(word);
+                    dfs(word, seq);
+                    seq.pop_back();
+                }
+            }
+            word[i] = original;
+        }
+    }
+
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-   unordered_set<string>st(wordList.begin(),wordList.end());
-   queue<vector<string>>q;
-   q.push({beginWord});
-   vector<string>usedonlev;
-   usedonlev.push_back(beginWord);
-   int level = 0;
-   vector<vector<string>>ans;
-   while(!q.empty())
-   {
-    vector<string>vec = q.front();
-    q.pop();
-    if(vec.size()>level)
-    {
-        level++;
-        for(auto it:usedonlev)
-        {
-            st.erase(it);
-        }
-        usedonlev.clear();
-    }
-    string word = vec.back();
-    if(word == endWord)
-    {
-        if(ans.size() == 0)
-        {
-            ans.push_back(vec);
-        }
-        else if(ans[0].size()==vec.size())
-        {
-            ans.push_back(vec);
-        }
-   }
-   for(int i=0;i<word.size();i++)
-   {
-    char orignal = word[i];
-    for(char c = 'a';c<='z';c++)
-    {
-        word[i] = c;
-        if(st.count(word)>0)
-        {
-            vec.push_back(word);
-            q.push(vec);
-            usedonlev.push_back(word);
-            vec.pop_back();
-        }
-    }
-    word[i] = orignal;
-   }
-   }
-   return ans;
-    }
-};*/
-class Solution
-{
-public:
-    vector<vector<string>> findLadders(string beginWord, string endWord,
-                                         vector<string> &wordList)
-    {
-        // Push all values of wordList into a set
-        // to make deletion from it easier and in less time complexity.
         unordered_set<string> st(wordList.begin(), wordList.end());
-        
-        // Creating a queue ds which stores the words in a sequence which is
-        // required to reach the targetWord after successive transformations.
-        queue<vector<string>> q;
+        if (st.find(endWord) == st.end()) return {}; // Early return if endWord not present
 
-        // BFS traversal with pushing the new formed sequence in queue 
-        // when after a transformation, a word is found in wordList.
+        queue<string> q;
+        b = beginWord;
+        sz = beginWord.size();
+        q.push(beginWord);
+        mpp[beginWord] = 1;
 
-        q.push({beginWord});
+        st.erase(beginWord);
+        while (!q.empty()) {
+            int size = q.size();
+            unordered_set<string> wordsThisLevel;
 
-        // A vector defined to store the words being currently used
-        // on a level during BFS.
-        vector<string> usedOnLevel;
-        usedOnLevel.push_back(beginWord);
-        int level = 0;
-       
-        // A vector to store the resultant transformation sequence.
-        vector<vector<string>> ans;
-        while (!q.empty())
-        {
-            vector<string> vec = q.front();
-            q.pop();
+            for (int k = 0; k < size; k++) {
+                string word = q.front(); q.pop();
+                int steps = mpp[word];
 
-            // Now, erase all words that have been
-            // used in the previous levels to transform
-            if (vec.size() > level)
-            {
-                level++;
-                for (auto it : usedOnLevel)
-                {
-                    st.erase(it);
-                }
-            }
-
-            string word = vec.back();
-
-            // store the answers if the end word matches with targetWord.
-            if (word == endWord)
-            {
-                // the first sequence where we reached end
-                if (ans.size() == 0)
-                {
-                    ans.push_back(vec);
-                }
-                else if (ans[0].size() == vec.size())
-                {
-                    ans.push_back(vec);
-                }
-            }
-            for (int i = 0; i < word.size(); i++)
-            {   
-                // Now, replace each character of ‘word’ with char
-                // from a-z then check if ‘word’ exists in wordList.
-                char original = word[i];
-                for (char c = 'a'; c <= 'z'; c++)
-                {
-                    word[i] = c;
-                    if (st.count(word) > 0)
-                    { 
-                        // Check if the word is present in the wordList and
-                        // push the word along with the new sequence in the queue.
-                        vec.push_back(word);
-                        q.push(vec);
-                        // mark as visited on the level
-                        usedOnLevel.push_back(word);
-                        vec.pop_back();
+                for (int i = 0; i < sz; i++) {
+                    char original = word[i];
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        word[i] = ch;
+                        if (st.count(word)) {
+                            if (mpp.find(word) == mpp.end()) {
+                                mpp[word] = steps + 1;
+                                q.push(word);
+                                wordsThisLevel.insert(word);
+                            }
+                        }
                     }
+                    word[i] = original;
                 }
-                word[i] = original;
+            }
+
+            for (auto& w : wordsThisLevel) {
+                st.erase(w); // Erase only after level finishes
             }
         }
+
+        if (mpp.find(endWord) != mpp.end()) {
+            vector<string> seq = { endWord };
+            dfs(endWord, seq);
+        }
+
         return ans;
     }
 };
