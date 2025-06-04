a43dada35da872beba061319fa928257af08c6e5
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-06-04 20:03:21 +0530

    Time: 7 ms (35.07%), Space: 17.4 MB (98.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
-   bool iscycledfs(int node,vector<bool>&visited,vector<bool>&ireccur,vector<int>&result,vector<int>adj[])
-   {
-    visited[node] = true;
-    ireccur[node] = true;
-    
-    for(auto neighbor:adj[node])
-    {
-        if(visited[neighbor]==false&&iscycledfs(neighbor,visited,ireccur,result,adj))
-        {
-            return true;
-        }
-        else if(ireccur[neighbor]== true)
-        {
-            return true;
-        }
-    }
-    ireccur[node] = false;
-    result.push_back(node);
-    return false;;
-   }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[numCourses];
-        for(const auto prerequisite:prerequisites)
-        {
-            int u = prerequisite[0];
-            int v = prerequisite[1];
-            adj[v].push_back(u);
-        }
-        vector<bool>visited(numCourses,false);
-        vector<bool>inreccur(numCourses,false);
         vector<int>result;
+        for(auto  &prerequisite:prerequisites)
+        {
+            int v = prerequisite[0];
+            int u = prerequisite[1];
+            adj[u].push_back(v);
+        }
+        vector<int>indegree(numCourses,0);
         for(int i =0;i<numCourses;i++)
         {
-        if(visited[i]==false&&iscycledfs(i,visited,inreccur,result,adj))
+            for(auto &p :adj[i])
+            {
+                indegree[p]++;
+            }
+        }
+        queue<int>q;
+        for(int i =0;i<numCourses;i++)
         {
-            return {};
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
         }
+       //3 simple bfs
+       while(!q.empty())
+       {
+        int curr = q.front();
+        q.pop();
+        result.push_back(curr);
+        for(int &v :adj[curr])
+        {
+            indegree[v]--;
+            if(indegree[v]==0)
+            {
+                q.push(v);
+            }
         }
-        reverse(result.begin(),result.end());
-        return result;
+       }
+       if(result.size()!=numCourses)
+       {
+        return {};
+       }
+       return result;
     }
 };
