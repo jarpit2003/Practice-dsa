f35d976ce9b2aaa36b8faefa9f9d2d2c3acaf083
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-06-11 18:00:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-vector<vector<int>>directions={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
 typedef pair<int,pair<int,int>>P;
+vector<vector<int>>directions={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m =grid.size();
-        int n = grid[0].size();
-        if(n==0||m==0||grid[n-1][m-1]!=0||grid[0][0]!=0) return -1;
-        vector<vector<int>>result(n,vector<int>(n,1e9));
-        priority_queue<P,vector<P>,greater<P>>pq;
-        pq.push({0,{0,0}});
-        result[0][0] = 0;
-        while(!pq.empty())
+      int m = grid.size();
+      int n = grid[0].size();
+      if(m==0||n==0||grid[m-1][n-1]!=0||grid[0][0]!=0) return -1;
+      priority_queue<P,vector<P>,greater<P>>pq;
+      vector<vector<int>>result(m,vector<int>(n,1e9));
+      result[0][0] = 1;
+      pq.push({1,{0,0}});
+      grid[0][0]=1;
+      int dist = grid[0][0];
+      while(!pq.empty())
+      {
+        int d = pq.top().first;
+        pair<int,int>node = pq.top().second;
+        int x = node.first;
+        int y = node.second;
+         pq.pop();
+        for(auto&dir:directions)
         {
-            int d = pq.top().first;
-            pair<int,int>node = pq.top().second;
-            int x = node.first;
-            int y = node.second;
-            pq.pop();
-            for(auto dir : directions)
+            int newx =x+dir[0];
+            int newy =y+dir[1];
+            dist = d+1;
+           
+            if(newx>=0&&newy>=0&&newx<m&&newy<n&&grid[newx][newy]==0&&d+1<result[newx][newy])
             {
-                int newx = x+dir[0];
-                int newy = y+dir[1];
-                int dist = 1;
-                if(newx>=0&&newx<n&&newy>=0&&newy<m&&grid[newx][newy]==0&&d+dist<result[newx][newy])
-                {
-                    pq.push({d+dist,{newx,newy}});
-                    result[newx][newy] = d+dist;
-                }
+                result[newx][newy] = dist;
+                pq.push({d+1,{newx,newy}});
+                grid[newx][newy] = 1;
             }
-        }
-        if(result[n-1][m-1]==INT_MAX) return -1;
-        return result[n-1][m-1]+1;
+            if(newx==m-1&&newy==n-1&&result[newx][newy]!=1e9)
+            {
+                return d+1;
+            }
+        } 
+      }
+      return -1;
     }
 };
