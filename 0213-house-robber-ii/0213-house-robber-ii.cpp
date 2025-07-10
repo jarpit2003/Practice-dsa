ae0177be7b789c707cc0018c4cae41d676de057d
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-07-11 03:10:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n+1,-1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i =2;i<=n-1;i++)
+        {
+            dp[i] = max(dp[i-1],nums[(i-1)]+dp[i-2]);
+        }
+       int result1 = dp[n-1];
+       dp.clear();
+       dp[0] = 0;
+       dp[1] = 0;
+       for(int i =2;i<=n;i++)
+        {
+            dp[i] = max(dp[i-1],nums[(i-1)]+dp[i-2]);
+        }
+        int result2 = dp[n];
+        return max(result1,result2);
+    }
+};
