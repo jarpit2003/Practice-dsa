ceabb54b61edeb1f4d3c155fbe7157b65ac0d112
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-08-21 19:20:32 +0530

    Time: 22 ms (46.39%), Space: 34.7 MB (79.19%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-typedef pair<int,int>p;
-vector<vector<int>>dir = {{-1,0},{0,-1},{1,0},{0,1}};
+vector<vector<int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+       queue<pair<int,int>>q;
         int m = mat.size();
         int n = mat[0].size();
-        queue<p>q;
-        for (int i =0;i<m;i++)
+        for(int i = 0;i<m;i++)
         {
-            for(int j =0;j<n;j++)
+            for(int j = 0;j<n;j++)
             {
-                if(mat[i][j]==0)  
+                if(mat[i][j]==0) q.push({i,j}) ;
+                else
                 {
-                q.push({i,j});
-                }  
-                else mat[i][j] = -1;          
+                    mat[i][j] = -1;
+                }
             }
         }
-       while(!q.empty())
-       {
-        p curr = q.front();
-        q.pop();
-        int i = curr.first;
-        int j = curr.second;
-        for(auto di:dir)
+        while(!q.empty())
         {
-            int newi = i+di[0];
-            int newj = j+di[1];
-            if(newi>=0&&newj>=0&&newi<m&&newj<n&&mat[newi][newj]==-1)
+            pair<int,int>curr = q.front();
+            q.pop();
+            for(auto&dir:directions)   
             {
-                  
-                    mat[newi][newj] = mat[i][j]+1;
-                    q.push({newi,newj});
-                  }
-            }
-        }     
-        return mat;
+                int new_x = curr.first+dir[0];
+                int new_y = curr.second+dir[1];
+                if(new_x>=0&&new_x<m&&new_y>=0&&new_y<n&&mat[new_x][new_y] == -1)
+                 {
+                    mat[new_x][new_y] = mat[curr.first][curr.second]+1;
+                    q.push({new_x,new_y});
+                 }
+            }     }
+            return mat;
     }
 };
