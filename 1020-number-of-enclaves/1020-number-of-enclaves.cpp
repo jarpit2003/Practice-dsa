80f84a0b827f452c4ee13a819ce9063b863fbb0e
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-09-03 10:24:50 +0530

    Time: 71 ms (11.08%), Space: 44.3 MB (11.15%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-vector<vector<int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
+typedef pair<int,int>p;
+vector<vector<int>>dir ={{0,1},{0,-1},{1,0},{-1,0}};
     int numEnclaves(vector<vector<int>>& grid) {
+        int cnt  = 0;
         int m = grid.size();
         int n = grid[0].size();
-        int count = 0;
-        queue<pair<int,int>>q;
-        for(int i = 0;i<m;i++)
+        queue<p>q;
+        for(int  i = 0;i<m;i++)
         {
             for(int j = 0;j<n;j++)
             {
-                if(grid[i][j] == 1) 
-                count++;
-                if((i == 0||i==m-1||j == 0||j == n-1)&&grid[i][j] == 1)
+                if(grid[i][j]==1) 
                 {
-                q.push({i,j});
-                grid[i][j] = 0;
-                count--;
+                    if(i==0||j==0||i==m-1||j==n-1) 
+                    {
+                        q.push({i,j});
+                        cnt++;
+                         grid[i][j] = 0;
+                    }
+                    else cnt++;
                 }
             }
         }
         while(!q.empty())
         {
-            pair<int,int>curr = q.front();
-            q.pop();
-            
-            int i = curr.first;
-            int j = curr.second;
-            for(auto dir:directions)
+          p cur = q.front();
+          int i= cur.first;
+          int j = cur.second;
+          q.pop();
+          cnt--;
+          for(auto d:dir)
+          {
+            int newi = i+d[0];
+            int newj = j+d[1];
+            if(newi>=0&&newi<m&&newj>=0&&newj<n&&grid[newi][newj]==1)
             {
-                int new_i = i + dir[0];
-                int new_j = j + dir[1];
-                if(new_i>=0&&new_i<m&&new_j>=0&&new_j<n&&grid[new_i][new_j] == 1)
-                {
-                    q.push({new_i,new_j});
-                    grid[new_i][new_j] = 0; 
-                    count--;
-                }
+                    q.push({newi,newj});
+                    grid[newi][newj] = 0;
             }
+          }
         }
-        return count;
+        return cnt;
     }
 };
