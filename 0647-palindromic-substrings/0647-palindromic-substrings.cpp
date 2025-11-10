f846a105c078654e577d1843d1ab9c35392c3bd4
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-11-10 16:16:25 +0530

    Time: 2 ms (91.73%), Space: 8.7 MB (65.39%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    bool ispal(string s)
-    {
-        bool flag = true;
-        int i = 0;
-        int j = s.size()-1;
-        while(i<j)
-        {
-            if(s[i]!=s[j]) 
-            {
-                flag = false;
-               break;
-            }
-            ++i; --j;  
-        }
-        return flag;
-    }
     int countSubstrings(string s) {
-        int count = 0;
-        for(int i =0;i<s.size();i++)
+        int n = (int)s.size();
+        if(n == 0) return 0;
+        int ans = 0;
+        for(int c = 0;c<n;c++)
         {
-            for(int j = i;j<s.size();j++)
+            int l = c;
+            int r = c;
+            while(l<=r&&l>=0&&r<n&&s[l]==s[r])
             {
-                if(ispal(s.substr(i,j-i+1))) count++;
+                 ans++;
+                 l--;
+                 r++;
             }
         }
-        return count;
+        for(int c = 0;c<n;c++)
+        {
+            int l = c;
+            int r = c+1;
+            while(l<=r&&l>=0&&r<n&&s[l]==s[r])
+            {
+                 ans++;
+                 l--;
+                 r++;
+            }
+        }
+        return ans;
     }
 };
