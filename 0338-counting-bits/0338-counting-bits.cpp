02f6a97314d128a3fa1a9b2deae40224dfed80ce
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-11-25 18:53:59 +0530

    Time: 0 ms (100%), Space: 10.5 MB (78.9%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-int count1sbin(const int &n)
-{
-    int ans = 0;
-    for(int i =0;i<32;i++)
-    {
-        if((n>>i)&1) ans++;
-    }
-    return ans;
-}
     vector<int> countBits(int n) {
-        vector<int>ans(n + 1);
-        for(int i =0;i<n+1;i++)
+        vector<int>dp(n+1,0);
+        for(int i = 0;i<=n;i++)
         {
-            ans[i] = count1sbin(i);
+            dp[i] = dp[i>>1]+(i&1);
         }
-        return ans;
+        return dp;
     }
 };
