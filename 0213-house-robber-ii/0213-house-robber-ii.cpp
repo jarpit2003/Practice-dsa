3306af89aa7941dd1bba2e4184d78947cd054509
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2025-11-25 18:20:56 +0530

    Time: 0 ms (100%), Space: 10.2 MB (88.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
+int roblinear(vector<int>& nums,int l,int r)
+{
+    int prev1 = 0;
+    int prev2 = 0;
+    for(int i =l;i<=r;i++)
+    {
+        int curr = max(prev1,prev2+nums[i]);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
     int rob(vector<int>& nums) {
+         if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
         int n = nums.size();
-        if(n==1) return nums[0];
-        vector<int>dp(n+1,-1);
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i =2;i<=n-1;i++)
-        {
-            dp[i] = max(dp[i-1],nums[(i-1)]+dp[i-2]);
-        }
-       int result1 = dp[n-1];
-       dp.clear();
-       dp[0] = 0;
-       dp[1] = 0;
-       for(int i =2;i<=n;i++)
-        {
-            dp[i] = max(dp[i-1],nums[(i-1)]+dp[i-2]);
-        }
-        int result2 = dp[n];
-        return max(result1,result2);
+        long case1 = roblinear(nums,0,n-2);
+        long case2 = roblinear(nums,1,n-1);
+        return (int)max(case1,case2);
     }
 };
