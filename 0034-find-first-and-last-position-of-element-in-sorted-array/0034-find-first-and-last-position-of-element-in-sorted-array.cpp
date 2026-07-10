59e69a04dea9e835c369db74ef15045dc65b4c52
Arpit Jain <118837874+jarpit2003@users.noreply.github.com>
2026-07-10 08:50:13 +0530

    Time: 0 ms (100%), Space: 17.6 MB (19.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int binarysearch(vector<int>nums,int target, bool first)
+    int search(vector<int>&nums,int target,bool first)
     {
         int low = 0;
-        int high = nums.size()-1;
-        int ans = -1;
-        while(low<=high)
+    int high = nums.size()-1;
+    int ans = -1;
+    while(low<=high)
+    {
+        int mid = low +(high-low)/2;
+        if(nums[mid]==target)
         {
-            int mid = low +(high-low)/2;
-            if(nums[mid]==target)
-            {
-                ans = mid;
-                if(first)
-                {
-                    high = mid-1;
-                }
-                else low = mid+1;
-            }
-            else if(nums[mid]<target) low = mid+1;
-            else high = mid-1;
+            ans = mid;
+            if(first) high = mid-1;
+            else low = mid+1;
         }
-        return ans;
+        else if(nums[mid]<target)low = mid +1;
+        else high = mid -1; 
+    }
+    return ans;   
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {binarysearch(nums,target,true),binarysearch(nums,target,false)};
+       return{search(nums,target,true),search(nums,target,false)};
     }
 };
